734a945e3cd1d43d0e746c077b3a325fda435b70
Marius Pop <mariuspopsebastian@gmail.com>
2017-01-19 20:26:02 +0200

    MLogin

--- src/test/java/org/fasttrackit/Automation/MLoginTest.java
+++ src/test/java/org/fasttrackit/Automation/MLoginTest.java
@@ -4,33 +4,51 @@ package org.fasttrackit.Automation;
 import org.fasttrackit.util.TestBase;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 
 public class MLoginTest extends TestBase {
     @Test
     public void validLogind(){
         driver.get("https://rawgit.com/sdl/Testy/master/src/test/functional/app-demo/login.html");
 
-        WebElement findEmail = driver.findElement(By.name("username"));
-        WebElement findPassword = driver.findElement(By.id("password"));
-        WebElement loginButton = driver.findElement(By.id("loginButton"));
-
-
-        findEmail.sendKeys("tu@fast.com");
-        findPassword.sendKeys("tu.pass");
-        loginButton.click();
+        login("eu@fast.com", "eu.pass");
 
 
         WebElement logoutButton = driver.findElement(By.linkText("Logout"));
         logoutButton.click();
 
+    }
+
+
+    @Test
+    public void invalidPasswordTest(){
+        driver.get("https://rawgit.com/sdl/Testy/master/src/test/functional/app-demo/login.html");
+
+        login("eu@fast.com", "eu.pass123");
+
+        WebElement errorElement = driver.findElement(By.className("error-msg"));
+        System.out.println(errorElement.getText());
+        //Assert.assertEquals(errorElement.getText(), "Invalid user or password!");
+        assertThat(errorElement.getText(), is ("Invalid user or password!"));
 
 
 
 
 
+    }
+
+    public void login(String user, String password){
+        WebElement findEmail = driver.findElement(By.name("username"));
+        WebElement findPassword = driver.findElement(By.id("password"));
+        WebElement loginButton = driver.findElement(By.id("loginButton"));
 
 
-
+        findEmail.sendKeys(user);
+        findPassword.sendKeys(password);
+        loginButton.click();
     }
 }
--- src/test/java/org/fasttrackit/Automation/MLoginTest2.java
+++ src/test/java/org/fasttrackit/Automation/MLoginTest2.java
@@ -1,7 +1,32 @@
 package org.fasttrackit.Automation;
 
-/**
- * Created by Marius on 14-Jan-17.
- */
-public class MLoginTest2 {
+import org.fasttrackit.util.TestBase;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+
+public class MLoginTest2 extends TestBase {
+    @Test
+    public void MLogin2(){
+
+       driver.get("https://rawgit.com/sdl/Testy/master/src/test/functional/app-demo/login.html");
+
+
+        WebElement findLoginButton = driver.findElement(By.className("login-btn"));
+        WebElement findEmail = driver.findElement(By.id("email"));
+        findLoginButton.click();
+        WebElement errormessage = driver.findElement(By.className("error-msg error"));
+
+
+
+
+
+
+
+
+
+
+    }
+
 }
